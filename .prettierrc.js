2af913dc66d3069c0327c7820eb533c82444c22a
Christian Diaz Portela <christiandp7@gmail.com>
2021-05-14 00:17:45 -0400

    image w video section created for difusers, ready to customize it

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+	printWidth: 80,
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+	tabWidth: 2,
+	semi: false,
+	bracketSpacing: false,
+	jsxBracketSameLine: true,
+	arrowParens: 'avoid',
+	endOfLine: 'auto',
+	embeddedLanguageFormatting: 'auto',
+}
