f2e552a30455127428497a59613a312bba315b9e
Christian Diaz Portela <christiandp7@gmail.com>
2021-06-26 21:02:02 -0400

    Setup postcss autoprefixer with sass:production npm command

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
+// const autoprefixer = require('autoprefixer')
+
 module.exports = {
-  plugins: [
-    require('autoprefixer'),
-    // require('postcss-nested')
-  ]
+	plugins: [
+		require('autoprefixer'),
+		// autoprefixer: {browsers: ['last 2 versions', 'iOS >= 8']},
+		// autoprefixer,
+	],
 }
